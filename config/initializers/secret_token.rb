7f9c5e49b8cfb1e28469fef255aee34238eace41
Pooja Jeyaprakash <poojajeyaprakash@gmail.com>
2013-08-01 15:11:37 -0400

    This is my first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ani::Application.config.secret_token = '493a0434ff080903478e6e12bf896e362b927e9a05c4d72731ac1990f294d46f8f6f0633cd1e6dc3a155270701ecfcc11d6cf940efbb52cf4b771f8264ba4312'
