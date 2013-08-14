67b4239e01ce92febbda34f6058fb776e9fba3d0
anishdeena <anishdeena@gmail.com>
2013-08-15 02:16:30 +0530

    count updated on each refresh

--- app/controllers/tictactoe_controller.rb
+++ app/controllers/tictactoe_controller.rb
@@ -6,7 +6,6 @@ class TictactoeController < ApplicationController
       auth = request.env["omniauth.auth"]
       if User.find_by_email(auth["info"]["email"])
         user = User.find_by_email(auth["info"]["email"])
-        user.num_login = user.num_login + 1
         user.save
         session[:email] = user.email
         session[:name] = user.name
@@ -15,7 +14,7 @@ class TictactoeController < ApplicationController
         user = User.new
         user.name = auth["info"]["name"]
         user.email = auth["info"]["email"]
-        user.num_login = 1
+        user.num_login = 0
         user.save
         session[:email] = user.email
         session[:name] = user.name
@@ -23,4 +22,14 @@ class TictactoeController < ApplicationController
       end
       redirect_to action:  :home
     end
+    
+    def updateCount
+      user = User.find_by_email(session[:email])
+      user.num_login = user.num_login + 1
+      user.save
+      session[:email] = user.email
+      session[:name] = user.name
+      session[:num_login] = user.num_login      
+    end
+    helper_method :updateCount
 end
